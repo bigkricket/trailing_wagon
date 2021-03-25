8e668ee6c03494bceb11bb32939b9d91780b6560
Ruben Perez <perezben37@gmail.com>
2021-03-24 19:22:00 -0700

    added in front_axle_controller

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -8,6 +8,7 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/rubaduben/github_robots/trailing_wagon/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/rubaduben/github_robots/trailing_wagon/devel/lib:$LD_LIBRARY_PATH"
 export PWD='/home/rubaduben/github_robots/trailing_wagon/build'
 export ROSLISP_PACKAGE_DIRECTORIES='/home/rubaduben/github_robots/trailing_wagon/devel/share/common-lisp'
 export ROS_PACKAGE_PATH="/home/rubaduben/github_robots/trailing_wagon/src:$ROS_PACKAGE_PATH"
---
+++ build/robot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/rubaduben/github_robots/trailing_wagon/src/robot_description
+# Build directory: /home/rubaduben/github_robots/trailing_wagon/build/robot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
